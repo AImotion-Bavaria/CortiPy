f13df1802b59451217ff8e9211b36725fa15c7cd
Jonas Heinzmann <jon03hei@gmail.com>
2026-01-15 13:20:11 +0100

    Alias assr_compute_psd import in assr.py
    Renamed the import of assr_compute_psd to _assr_compute_psd and created a local alias assr_compute_psd. This change may help avoid naming conflicts or clarify usage within the module.

--- cortipy/evaluation/assr.py
+++ cortipy/evaluation/assr.py
@@ -10,7 +10,9 @@ import matplotlib.pyplot as plt
 import mne
 
 from cortipy.evaluation.base import EvaluatorBase, save_new_figures
-from cortipy.shared import assr_calc_snr, assr_compute_psd, assr_f_test, calc_fft, plot_cortipy_topomap, plot_assr_spectrum
+from cortipy.shared import assr_calc_snr, assr_compute_psd as _assr_compute_psd, assr_f_test, calc_fft, plot_cortipy_topomap, plot_assr_spectrum
+
+assr_compute_psd = _assr_compute_psd
 
 
 class AssrEvaluator(EvaluatorBase):
